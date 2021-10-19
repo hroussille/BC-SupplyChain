87853dc05448828260ddb82b15127181d1b5b58c
Hector Roussille <hector.roussille@gmail.com>
2021-10-19 18:33:01 +0200

    Fixed error in truffle config file

--- app/truffle-config.js
+++ app/truffle-config.js
@@ -54,7 +54,6 @@ module.exports = {
     },
 
     developmentCLI: {
-      development: {
         host: "127.0.0.1",     // Localhost (default: none)
         port: 8545,            // Standard Ethereum port (default: none)
         network_id: "*",       // Any network (default: none)
